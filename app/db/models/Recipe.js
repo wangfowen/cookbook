c9246b1695d14420a4828f90dfee856bcc704197
Owen Wang <wangfowen@gmail.com>
2019-02-18 16:39:08 +1300

    figured out format for recipe data

--- app/db/models/Recipe.js
+++ app/db/models/Recipe.js
@@ -14,21 +14,62 @@ Recipe.schema = {
 Recipe.loadRecipes = () => {
   /*
   in the future, this should load from an endpoint if there's internet to get the latest data
-  for now, it'll get it from a JSON file
+  for now, it'll get it from a file
   */
 
-  //TODO: think about what JSON data need to look like to render
   return new Promise((resolve, reject) => {
     resolve([
       {
         key: "1",
-        title: "test",
-        ingredients: "blah, blah"
-      },
-      {
-        key: "2",
-        title: "test 2",
-        ingredients: "blah, blah"
+        title: "Fancy Avocado",
+        hero_image: "link",
+        ingredients: {
+          main: [
+            {
+              amount: 1,
+              size: "large",
+              name: "Avocado",
+              info: "Use a ripe one"
+            }, {
+              amount: 0.5,
+              name: "Lemon"
+            }
+          ],
+          garnish: [
+            {
+              name: "Black Pepper",
+              info: "Freshly ground is better"
+            }
+          ],
+          basics: [
+            {
+              name: "Salt"
+            }
+          ]
+        },
+        cook_min: 10,
+        prep_min: 5,
+        servings: 2,
+        tools: ["Knife", "Cutting Board"],
+        steps: {
+          prep: [
+            {
+              info: "Cut [avocado](Avocado), remove core, cut into slices",
+              tools: ["Knife", "Cutting Board"],
+              output: "Sliced Avocado"
+            }
+          ],
+          cook: [
+            {
+              info: "Squeeze [lemon](Lemon) on [avocado](Sliced Avocado). Add {[salt](Salt) and [pepper](Black Pepper)}(1)",
+              tools: ["Knife"],
+              output: "Fancy Avocado"
+            }
+          ]
+        },
+        notes: {
+          1: "Salt and pepper to taste."
+        }
       }
     ]);
   });
